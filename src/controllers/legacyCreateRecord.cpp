1a2e9b60f49e4c4957f5f389a6c23a2ad34fe47f
SonNguyen <nguyenthaison0201@gmail.com>
2024-04-12 13:37:18 +0700

    legacy server

--- src/controllers/legacyCreateRecord.cpp
+++ src/controllers/legacyCreateRecord.cpp
@@ -2,7 +2,7 @@
 
 HANDLER legacyCreateRecord = [](Request *req, Response *res, bool *next) {
     String id = req->value("esp-id"), vol = req->value("vol"), timeStr = req->value("time");
-
+    // std::cout<<req->rawText()<<std::endl;
     int time = stoi(timeStr);
     pirOrm.createRecord(id, vol, time);
     res->asPlainText("OK");
